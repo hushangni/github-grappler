377152dec9befb2caa1e34a27cfd4266c5ca561f
Shangni <shangnihu@gmail.com>
2019-08-11 14:53:13 -0400

    Set up search bar and initial state of search state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import { createStore } from 'redux';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+// creating initial search state
+const INIT_SEARCH_STATE = {
+  githubName: ""
+};
 
-    </div>
+// @action
+// set search name
+const setSearchName = name => ({
+  type: "SET_SEARCH_NAME",
+  payload: { name }
+});
+
+
+// @reducer
+// search githubname reducer
+const searchGithubNameReducer = (state = INIT_SEARCH_STATE, action) => {
+  switch (action.type) {
+    case "SET_SEARCH_NAME":
+      return { ...state, githubName: action.payload.name };
+    default:
+      return state;
+  }
+};
+
+// @store
+// creating global store
+export const store = createStore(searchGithubNameReducer);
+
+const SearchBar = () => {
+  return (
+    <section>
+      <input
+        type="text"
+        // value={pokemonName}
+        // onChange={evt => setNewName(evt.target.value)}
+      />
+
+      <button type="button">Search</button>
+    </section>
   );
-}
+};
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <main>
+        <SearchBar />
+      </main>
+    );
+  }
+};
 
 export default App;
+
